1714fd8f54ec262574943f79a65d2de3b3666875
Stefan Schleich <stefan@plusx.de>
2019-05-16 10:41:15 +0200

    Added composer.json

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,26 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
+namespace RackRuether\MailChimpBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use RackRuether\MailChimpBundle\RackRuetherMailChimpBundle;
 
-class ContaoManagerPlugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
      */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create('RackRuether\MailchimpBundle\RackRuetherMailchimpBundle')
-                ->setLoadAfter([
-                    'Contao\CoreBundle\ContaoCoreBundle',
-                    'Oneup\Contao\MailChimpBundle'
-                ]),
-            BundleConfig::create('RackRuether\ProductBundle\RackRuetherProductBundle')
-                ->setLoadAfter([
-                    'Contao\CoreBundle\ContaoCoreBundle'
-                ]),
+            BundleConfig::create(RackRuetherMailChimpBundle::class)->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
 }
