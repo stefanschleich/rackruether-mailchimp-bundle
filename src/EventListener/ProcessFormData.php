cafb436fcb9db35ecb74f0f0a4f11a4b9419d20c
Stefan Schleich <schleich@plusx.de>
2023-05-05 08:49:19 +0000

    Allow second subscribe

--- src/EventListener/ProcessFormData.php
+++ src/EventListener/ProcessFormData.php
@@ -11,28 +11,52 @@ class ProcessFormData
     {
         //$formID = $objForm->getFormId();
 
-        if($GLOBALS['TL_CONFIG']['rrMailchimpApiKey'] && $arrPost['MailchimpListID'] && $arrPost['Newsletter'] == 1 && $arrPost['Datenschutz'] == 1) {
+        if($GLOBALS['TL_CONFIG']['rrMailchimpApiKey'] && $arrPost['MailchimpListID']) {
 
             $mc = new Client($GLOBALS['TL_CONFIG']['rrMailchimpApiKey']);
 
-            try {
-                $subscribed = $mc->subscribeToList(
-                    $arrPost['MailchimpListID'],           // List ID
-                    $arrPost['Email'],          // E-Mail address
-                    array_filter([                       // Array with first/lastname (MailChimp merge tags)
-                        'FNAME' => $arrPost['Vorname'],
-                        'LNAME' => $arrPost['Nachname'],
-                        'DSGVO' => 1
-                    ]),
-                    true                    // Double opt-in true
-                );
-                if ($subscribed) {
-                    // throw new \Exception('SUBSCRIBED: ' . $subscribed);
-                } else {
-                    // throw new \Exception('NOT SUBSCRIBED: ' . $arrPost['Vorname']);
+            if($arrPost['Newsletter'] == 1) {
+                try {
+                    $subscribed = $mc->subscribeToList(
+                        $arrPost['MailchimpListID'],           // List ID
+                        $arrPost['Email'],          // E-Mail address
+                        array_filter([                       // Array with first/lastname (MailChimp merge tags)
+                            'FNAME' => $arrPost['Vorname'],
+                            'LNAME' => $arrPost['Nachname'],
+                            'DSGVO' => 1
+                        ]),
+                        true                    // Double opt-in true
+                    );
+                    if ($subscribed) {
+                        // throw new \Exception('SUBSCRIBED: ' . $subscribed);
+                    } else {
+                        // throw new \Exception('NOT SUBSCRIBED: ' . $arrPost['Vorname']);
+                    }
+                } catch (ApiException $e) {
+    
                 }
-            } catch (ApiException $e) {
-
+            }
+            
+            if($arrPost['Teilnahmebedingungen'] == 1 && $arrPost['MailchimpListID2']) {
+                try {
+                    $subscribed_second = $mc->subscribeToList(
+                        $arrPost['MailchimpListID2'],           // List ID
+                        $arrPost['Email'],          // E-Mail address
+                        array_filter([                       // Array with first/lastname (MailChimp merge tags)
+                            'FNAME' => $arrPost['Vorname'],
+                            'LNAME' => $arrPost['Nachname'],
+                            'DSGVO' => 1
+                        ]),
+                        true                    // Double opt-in true
+                    );
+                    if ($subscribed_second) {
+                        // throw new \Exception('SUBSCRIBED: ' . $subscribed);
+                    } else {
+                        // throw new \Exception('NOT SUBSCRIBED: ' . $arrPost['Vorname']);
+                    }
+                } catch (ApiException $e) {
+                
+                }
             }
         }
     }
